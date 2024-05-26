d9ea444845d5a51e30e97177cef19bb24d6f9a55
zkfarmoor <166660193+zkfarmoor@users.noreply.github.com>
2024-05-26 18:18:00 +0000

    add chains

--- src/consts/chains.ts
+++ src/consts/chains.ts
@@ -95,4 +95,73 @@ export const chains: ChainMap<ChainMetadata & { mailbox?: Address }> = {
       gasPrice: 0.1,
     },
   },
+  // Including configs for some Solana chains by default
+  solana: {
+    ...chainMetadata.solana,
+    rpcUrls: [
+      {
+        http: process.env.NEXT_PUBLIC_SOLANA_RPC_URL || 'https://api.mainnet-beta.solana.com',
+      },
+    ],
+    mailbox: 'Ge9atjAc3Ltu91VTbNpJDCjZ9CFxFyck4h3YBcTF9XPq',
+  },
+  solanatestnet: {
+    ...chainMetadata.solanatestnet,
+    mailbox: 'TODO',
+  },
+  solanadevnet: {
+    ...chainMetadata.solanadevnet,
+    mailbox: '4v25Dz9RccqUrTzmfHzJMsjd1iVoNrWzeJ4o6GYuJrVn',
+  },
+  proteustestnet: {
+    chainId: 88002,
+    domainId: 88002,
+    name: 'proteustestnet',
+    protocol: ProtocolType.Ethereum,
+    displayName: 'Proteus Testnet',
+    displayNameShort: 'Proteus',
+    nativeToken: {
+      name: 'Zebec',
+      symbol: 'ZBC',
+      decimals: 18,
+    },
+    rpcUrls: [
+      {
+        http: 'https://api.proteus.nautchain.xyz/solana',
+      },
+    ],
+    blockExplorers: [
+      {
+        name: 'Proteus Explorer',
+        url: 'https://proteus.nautscan.com/proteus',
+        apiUrl: 'https://proteus.nautscan.com/proteus',
+        family: ExplorerFamily.Other,
+      },
+    ],
+    mailbox: '0x918D3924Fad8F71551D9081172e9Bb169745461e',
+  },
+  nautilus: {
+    chainId: 22222,
+    domainId: 22222,
+    name: 'nautilus',
+    protocol: ProtocolType.Ethereum,
+    displayName: 'Nautilus',
+    logoURI: 'https://www.nautchain.xyz/media/nuatchain_media_kit/naut_sq.png',
+    nativeToken: {
+      name: 'Zebec',
+      symbol: 'ZBC',
+      decimals: 18,
+    },
+    rpcUrls: [
+      {
+        http: 'https://api.nautilus.nautchain.xyz',
+      },
+    ],
+    blocks: {
+      confirmations: 1,
+      reorgPeriod: 1,
+      estimateBlockTime: 1,
+    },
+    mailbox: '0xF59557dfacDc5a1cb8A36Af43aA4819a6A891e88',
+  },
 };
